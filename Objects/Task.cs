2f4e8646669feb0c19dc8dfca3aaf9bd28f1330d
Chris Kuiper and Matt Newell <chris.kuiper@gmail.com and mnewell10@gmail.com>
2016-02-10 09:03:03 -0800

    add get and sets for g=tasks

--- Objects/Task.cs
+++ Objects/Task.cs
@@ -1,1 +1,43 @@
 using System.Collections.Generic;
+
+namespace ToDoList
+{
+  public class Task
+  {
+    private string _description;
+    private int _id;
+    private static List<Task> _instances = List<Task> {};
+
+    public Task(string descrition)
+    {
+      _description = description;
+      _instances.Add(this);
+      _id = _instances.Count;
+    }
+
+    public string GetDescription()
+    {
+      return _description;
+    }
+    public void SetDescription(string newDescription)
+    {
+      _description = newDescription;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public static List<Task> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static Task Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+  }
+}
